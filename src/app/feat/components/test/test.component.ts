328a0868989e71de4cb518012b2105563e0bf28e
tsizehena223 <tsizehena223@gmail.com>
2024-02-25 08:04:38 +0300

    fix: typo

--- src/app/feat/components/test/test.component.ts
+++ src/app/feat/components/test/test.component.ts
@@ -30,21 +30,18 @@ export class TestComponent implements AfterViewInit, OnInit {
   test_nombre_quality(container: any, identiant: string, test: any) {
     const div = container.nativeElement.querySelector(`#${identiant}`);
     console.log(div);
-    
+
     const elt = div.querySelectorAll('.elt');
     if (elt.length <= 4) { // si le npmbre de qualite dans le card est a 3
       console.log(elt.length);
-      
+
       this.renderer.addClass(div, 'btn_disable') // on ajoute la calsse btn_disable a cette element
       const alert = this.renderer.createElement('span') // on cre une elt span
-      alert.innerHTML = `3 Qualité  à chaque type <svg xmlns="http://www.w3.org/2000/svg" class="bi bi-exclamation-triangle-fill" viewBox="0 0 16 16">
+      alert.innerHTML = `3 elements for each type <svg xmlns="http://www.w3.org/2000/svg" class="bi bi-exclamation-triangle-fill" viewBox="0 0 16 16">
         <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5m.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2"/>
       </svg>`
       this.renderer.addClass(alert, 'alert')// on ajoute une classe alerte sur les span
       this.renderer.appendChild(test.nativeElement, alert) //  on attache l'elt a son parent
-      setTimeout(() => { // on attend 3s on supprimer l'elt
-        this.renderer.removeChild(test.nativeElement, alert)
-      }, 3000)
     }
   }
 
@@ -79,14 +76,14 @@ export class TestComponent implements AfterViewInit, OnInit {
 
   }
   ngAfterViewInit(): void {
-      setTimeout(() => {    
-        this.selecteQuality(this.listquality, this.myquality, this.testContainer)
-        this.selecteQuality(this.listquality2, this.myquality, this.testContainer)
-        this.selecteQuality(this.listquality3, this.myactivity, this.testContainer1)
-        this.selecteQuality(this.listquality4, this.myactivity, this.testContainer1)
-        this.selecteQuality(this.listquality5, this.myprofession, this.testContainer2)
-        this.selecteQuality(this.listquality6, this.myprofession, this.testContainer2)
-      }, 4000);
+    setTimeout(() => {
+      this.selecteQuality(this.listquality, this.myquality, this.testContainer)
+      this.selecteQuality(this.listquality2, this.myquality, this.testContainer)
+      this.selecteQuality(this.listquality3, this.myactivity, this.testContainer1)
+      this.selecteQuality(this.listquality4, this.myactivity, this.testContainer1)
+      this.selecteQuality(this.listquality5, this.myprofession, this.testContainer2)
+      this.selecteQuality(this.listquality6, this.myprofession, this.testContainer2)
+    }, 4000);
   }
 
   // Firebase
@@ -149,6 +146,6 @@ export class TestComponent implements AfterViewInit, OnInit {
 
   ngOnInit(): void {
     this.refreshActivities();
-    
+
   }
 }
